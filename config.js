a622a4ea311d3c311aac30b89773d93f0181d9b9
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-06-14 14:13:41 -0500

    fix some vulnerable information, add env file to read env variables

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+const dotnev = require("dotenv");
+dotnev.config();
+
 const config = {
-  dbUrl:
-    process.env.DB_URL ||
-    "mongodb+srv://messageAppNode:gmtmdq5Tk6Wwq1rz@cluster0.n14oscc.mongodb.net/platziMessage_db",
+  dbUrl: process.env.DB_URL,
   port: process.env.PORT || 3000,
   host: process.env.HOST || "http://localhost",
   publicRoute: process.env.PUBLIC_ROUTE || "/app",
